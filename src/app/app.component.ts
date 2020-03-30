45b01e911f88ad222cd20b4b6cd2febb7809002e
Matthias Haegele <matthias.haegele@mhp.com>
2020-03-30 14:40:25 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample-integration-angular';
+  clickMessage = '';
+  selectInput = 'Headline A';
+  checkBox = false;
+  radioButton = false;
+  textFieldHeadline = 'Change me';
+  textValue = '';
+  activePage = 1;
+
+  onButtonClick() {
+    this.clickMessage = 'Headline appears through Button click';
+  }
+
+  onButtonPureClick() {
+    this.clickMessage = '';
+  }
+
+  handleSelectChange(event) {
+    this.selectInput = event.target.value;
+  }
+
+  handleCheckbox() {
+    this.checkBox = this.checkBox === false;
+  }
+
+  handleRadio() {
+    this.radioButton = this.radioButton === false;
+  }
+
+  handleTextField(event: KeyboardEvent) {
+    this.textValue = (event.target as HTMLInputElement).value;
+    if (this.textValue === '') {
+      this.textFieldHeadline = 'Change me';
+    }else {
+      this.textFieldHeadline = this.textValue;
+    }
+  }
 }
