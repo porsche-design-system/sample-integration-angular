637a52d38d28a116f19cc03f14208eacd9cfdbf3
Bernd Hacker <hacker.bernd@gmail.com>
2020-05-25 12:43:31 +0200

    fix: make phn-header full width | bh

--- src/app/pages/page-with-phn-header.component.ts
+++ src/app/pages/page-with-phn-header.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'page-with-phn-header',
   template: `
-    <phn-header></phn-header>
-    <div style="paddingTop: 20px">
+    <phn-header style="position: absolute; left: 0; right: 0;"></phn-header>
+
+    <div style="paddingTop: 150px">
       <div>
         <p-button>Some PButton</p-button>
       </div>
