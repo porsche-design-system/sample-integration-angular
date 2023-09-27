584a0077eec9d419beb49587b7f2101439dd20b0
TatjanaGartner <tatjana.gartner@mhp.com>
2023-09-27 10:44:27 +0200

    Revert "fix: try to fix partials in test mode | tg"
    This reverts commit 69dc7e09fea21eccbe55f5f5ece2ee270388e9e5.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,10 +11,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  afterAll(() => {
-    TestBed.resetTestingModule();
-  });
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
