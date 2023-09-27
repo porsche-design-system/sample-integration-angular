13dbeaa3cc26da0c4db1be2e175373d99ac63817
TatjanaGartner <tatjana.gartner@mhp.com>
2023-09-27 10:52:12 +0200

    fix: apply partials correctly | tg / bh

--- scripts/transformIndexHtml.js
+++ scripts/transformIndexHtml.js
@@ -1,20 +1,19 @@
 const partials = require('@porsche-design-system/components-angular/partials');
 
+// https://github.com/just-jeb/angular-builders/blob/fdd7c8ed00b7eb7e1761aaa6cb5bda41693ceb5d/packages/custom-webpack/README.md#index-transform
 module.exports = (targetOptions, indexHtml) => {
   const headPartials = [
-    partials.getComponentChunkLinks({ components: ['button', 'marque'] }),
-    partials.getInitialStyles({}),
-    partials.getFontFaceStylesheet(),
-    partials.getFontLinks(),
-    partials.getIconLinks({ icons: ['arrow-head-right', 'plus'] }),
-    partials.getMetaTagsAndIconLinks({ appTitle: 'Sample Integration Angular' }),
-  ].join('\n');
+    partials.getInitialStyles(),
+    partials.getFontLinks({ weights: ['regular', 'semi-bold', 'bold'] }),
+  ]
+    .join('\n')
+    .replace(/https:\/\/cdn\.ui\.porsche\.com\/porsche-design-system/g, 'http://localhost:3001');
 
-  const bodyPartials = [
-    partials.getLoaderScript(),
-    partials.getCookiesFallbackScript(),
-    partials.getBrowserSupportFallbackScript(),
-  ].join('\n');
+  const bodyPartials = [partials.getBrowserSupportFallbackScript(), partials.getCookiesFallbackScript()]
+    .join('\n')
+    .replace(/https:\/\/cdn\.ui\.porsche\.com\/porsche-design-system/g, 'http://localhost:3001');
 
-  return indexHtml.replace(/(<\/head>)/, `\n${headPartials}$1`).replace(/(<\/body>)/, `\n${bodyPartials}$1`);
+  console.log('Injected partials via transformIndexHtml');
+
+  return indexHtml.replace(/<\/head>/, `\n${headPartials}$&`).replace(/<\/body>/, `\n${bodyPartials}$&`);
 };
