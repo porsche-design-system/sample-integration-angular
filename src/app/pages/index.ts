1da5417b1eac660a20dd191cf4918d44265cd397
Stephan Schröter <stephan.schroeter@me.com>
2025-01-23 10:37:44 +0100

    update porsche design system to 3.25.0-rc.1 and remove utilties package | sas

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,12 +2,10 @@ import { CollectionPage } from './collection.page';
 import { FormsPage } from './forms.page';
 import { PerformancePage } from './performance.page';
 import { PhnHeaderPage } from './phn-header.page';
-import { UtilitiesPage } from './utilities.page';
 
-export const pages = [CollectionPage, FormsPage, PerformancePage, PhnHeaderPage, UtilitiesPage];
+export const pages = [CollectionPage, FormsPage, PerformancePage, PhnHeaderPage];
 
 export * from './collection.page';
 export * from './forms.page';
 export * from './performance.page';
 export * from './phn-header.page';
-export * from './utilities.page';
